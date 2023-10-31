77b69aabcfefb4fde32886560b2afed2f132efc8
suhailpm <tosuhailpm@gmail.com>
2023-10-31 13:21:21 +0530

    advancements

--- actions/index.js
+++ actions/index.js
@@ -1,11 +1,35 @@
-
 export const fetchAllParticipants = async () => {
-    try {
-      const response = await fetch("api/participants/all");
-      const data = await response.json();
-      console.log( data.allParticipants, ' are all participants');
-      return data.allParticipants
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  try {
+    const response = await fetch("api/participants/all");
+    const data = await response.json();
+    console.log(data.allParticipants, " are all participants");
+    return data.allParticipants;
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const updateParticipant = async (id, formData) => {
+  // console.log(id, formData, ' from actions');
+  try {
+    const response = await fetch(`api/participants/edit/${id}`, {
+      method: "POST",
+      body: formData,
+    });
+    const data = await response.json(); //{data: 'particpant object', message: 'successflly updated'}
+    return data
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const addParticipant = async (data) => {
+  try {
+  const response = await fetch(`api/participants/add`, {
+    method: "POST",
+    body: JSON.stringify(data),
+  });
+  const data = await response.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
---
+++ app/api/participants/edit/[id]/route.js
@@ -0,0 +1,15 @@
+import connectToDb from '@utils/connectToDb'
+import Participant from '@models/Participant';
+export const POST = async (req,{params}) => {
+    try {
+        const body = await req.json()
+        console.log(body,' is body in backed post request')
+        await connectToDb()
+        const updatedParticipant = await Participant.findByIdAndUpdate(params.id, body,{new: true})
+        console.log(updatedParticipant);
+        return new Response(JSON.stringify({data:updatedParticipant, message:'Successfully updated the details'}),{status: 200})
+
+    } catch (error) {
+        // console.log(error);
+    }
+}
