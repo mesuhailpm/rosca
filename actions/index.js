eeec1ac307cd29ce920d484f7a40716e94e6bf72
suhailpm <tosuhailpm@gmail.com>
2023-11-05 07:36:40 +0530

    added logs

--- actions/index.js
+++ actions/index.js
@@ -2,53 +2,53 @@ export const fetchAllParticipants = async () => {
   try {
     const response = await fetch("api/participants/all");
     const data = await response.json();
-    //console.log(data.allParticipants, " are all participants");
+    console.log(data.allParticipants, " are all participants");
     return data.allParticipants;
   } catch (error) {
-    //console.log(error);
+    console.log(error);
   }
 };
 export const updateParticipant = async (id, formData) => {
-  //console.log(id, formData, ' from updateParticipant actions');
+  console.log(id, formData, ' from updateParticipant actions');
   try {
     const response = await fetch(`api/participants/edit/${id}`, {
       method: "POST",
       body: formData,
     });
     const data = await response.json(); //{data: 'particpant object', message: 'successflly updated'}
-    //console.log(data, 'from updateParticipant It should contain the error');
+    console.log(data, 'from updateParticipant It should contain the error');
     return data;
   } catch (error) {
-    //console.log(error);
+    console.log(error);
   }
 };
 
 export const addParticipant = async (formData) => {
-  //console.log(formData);
+  console.log(formData);
   try {
     const response = await fetch(`api/participants/add`, {
       method: "POST",
       body: JSON.stringify(formData),
     });
     const data = await response.json(); //{data: 'particpant object', message: 'successflly updated'}
-    //console.log(data, ' is the result after creating a new participant');
+    console.log(data, ' is the result after creating a new participant');
     return data;
   } catch (error) {
-    //console.log(error);
+    console.log(error);
   }
 };
 
 export const deleteParticipant = async (id) => {
-  //console.log(id,' got in action');
+  console.log(id,' got in action');
   try {
     const response = await fetch(`api/participants/delete/${id}`, {
       method: "DELETE",
     });
     const data = await response.json();//{data: 'particpant object', message: 'successflly updated'}
-    //console.log(data, ' is data returned');
+    console.log(data, ' is data returned');
     return data;
 
   } catch (error) {
-    //console.log(error);
+    console.log(error);
   }
 };
--- app/api/participants/delete/[id]/route.js
+++ app/api/participants/delete/[id]/route.js
@@ -4,9 +4,9 @@ import connectToDb from "@utils/connectToDb";
 export const DELETE = async (req, { params }) => {
   try {
     await connectToDb();
-    //console.log(params.id, "got in backend");
+    console.log(params.id, "got in backend");
     const deletedParticipant = await Participant.findByIdAndRemove(params.id);
-    //console.log(deletedParticipant);
+    console.log(deletedParticipant);
     return new Response(
       JSON.stringify({ result: deletedParticipant,message: "Member removed successfully" })
     );
