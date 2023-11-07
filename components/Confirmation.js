15b5bdeb5feb5432afce359c91c8a79cc107e369
suhailpm <tosuhailpm@gmail.com>
2023-11-07 07:23:13 +0530

    minimize icon size. efficient code

--- components/Confirmation.js
+++ components/Confirmation.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import completeIcon from 'public/assets/images/complete-icon.png'
-import failedIcon from 'public/assets/images/failed-icon.png'
+
+import errorIcon from 'public/assets/images/incorrect-icon.png'
 import Image from 'next/image';
 
 const Confirmation = ({confirmationMessage}) => {
   const {success, message} = confirmationMessage
+  const icon = success ? completeIcon : errorIcon
   return (
     <div className='flex flex-col justify-center items-center border border-4 p-2 h-40 border-black rounded-xl gap-4 font-bold bg-white/75
      left-1/2 top-1/2
@@ -12,7 +14,7 @@ const Confirmation = ({confirmationMessage}) => {
       '
       >
         <h1>{`${message}!`}</h1> 
-        <Image src={success ? completeIcon: failedIcon} width={40} className='border border-black rounded-full' alt='success logo'/>
+        <Image src={icon} width={40} className='border border-black rounded-full' alt='success logo'/>
 
     </div>
   )
