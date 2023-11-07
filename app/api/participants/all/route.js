8f25b7e7a6ac88897ba793ef03a9b69d8673090e
suhailpm <tosuhailpm@gmail.com>
2023-11-07 13:32:16 +0530

    log

--- app/api/participants/all/route.js
+++ app/api/participants/all/route.js
@@ -4,11 +4,12 @@ export const GET = async () => {
     try {
         await connectToDb()
         const allParticipants = await Participant.find()
-        // console.log(allParticipants)
+        console.log(allParticipants, ' inside GET')
         return new Response(JSON.stringify({allParticipants,success: true, status: 200}))
         
     } catch (error) {
-        return new Response(error, {success: false, status:405})
+        console.log(error, ' error in route GET');
+        return new Response(JSON.stringify({error}), {success: false, status:405})
 
     }
 
