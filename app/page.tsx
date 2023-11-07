7fe096b435f1307d823e4e1a6ecf8a0d5cf14eba
suhailpm <tosuhailpm@gmail.com>
2023-11-07 08:13:47 +0530

    useStore

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
-"use client";
-import React, { useEffect, useState } from "react";
+'use client'
+import {useStore} from '@src/store';
+import {useState, useEffect } from 'react'
+import { fetchAllParticipants } from '@actions';
 
-
-import { fetchAllParticipants } from '@actions'
 
 export default function Home() {
-  interface Participant {
+  const {participants, setParticipants} = useStore()
+
+
+  type Participant = {
     name: string;
     serial: number;
     claimed: boolean;
@@ -15,34 +18,32 @@ export default function Home() {
     participants: Participant[];
     winnerToBeDeclared: string;
   }
+      const fetchParticipants = async ()=>{
+        try {
+          const fetchedParticipants = await fetchAllParticipants();
+          fetchedParticipants.sort((a:Participant, b:Participant) => a.serial - b.serial)
+          useStore.setState({participants: fetchedParticipants});
+          
+        } catch (error) {
+          console.log(error);
+          
+        }
+      }
+
 
   // const [participant, setParticipant] = useState<IndexState['participant']>('')
-  const [participants, setParticipants] = useState<IndexState["participants"]>(
-    []
-  )
-  const [loading, setLoading] = useState(true)
-
-
   
+  useEffect(()=>{
+    fetchParticipants()
+  },[])
 
-
-  
-  useEffect(() => {
-    (async () => {
-      const participants = await fetchAllParticipants();
-      participants.sort((a:Participant, b:Participant) => a.serial - b.serial)
-      setParticipants(participants);
-      setLoading(false);
-    })()
-
-  }, []);
 
   return (
-    <div className="flex flex-col items-center pt-4 bg-yellow-100 h-screen">
+    <div className="flex flex-col items-center pt-4 bg-yellow-100 h-full">
       <h1 className="font-bold text-3xl">ROSCA Wheel</h1>
       
       <h1 className="text-blue-800 font-bold text-2xl pt-4">Participants</h1>
-      {loading && (<h1>Loading members list</h1>)}
+      {/* {loading && (<h1>Loading members list</h1>)} */}
       {participants.length > 0 && (
         <table className="self-start m-4 bg-[#96D4D4] border border-black table-auto font-raleway">
           <thead>
@@ -53,7 +54,7 @@ export default function Home() {
             </tr>
           </thead>
           <tbody>
-            {participants.map((participantObject, index) => (
+            {participants.map((participantObject:Participant, index:number) => (
               <tr key={index} className={`${index % 2 === 1 ? '' : 'bg-green-300'}`}>
                 <td>{participantObject.serial}</td>
                 <td>{participantObject.name}</td>
