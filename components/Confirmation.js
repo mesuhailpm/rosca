8dffc2e0ea81d9b329c2e6bea75a1f5599eb8333
suhailpm <tosuhailpm@gmail.com>
2023-11-08 13:40:10 +0530

    done

--- components/Confirmation.js
+++ components/Confirmation.js
@@ -8,7 +8,7 @@ const Confirmation = ({confirmationMessage}) => {
   const {success, message} = confirmationMessage
   const icon = success ? completeIcon : errorIcon
   return (
-    <div className='flex flex-col justify-center items-center border border-4 p-2 h-40 border-black rounded-xl gap-4 font-bold bg-white/75 left-1/2 top-1/2'
+    <div className='fixed flex flex-col justify-center items-center border border-4 p-2 h-40 border-black rounded-xl gap-4 font-bold bg-white/75  self-center'
       >
         <h1>{`${message}!`}</h1> 
         <Image src={icon} width={40} className='border border-black rounded-full' alt='success/failur logo'/>
--- components/Spinner.js
+++ components/Spinner.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import {Watch} from 'react-loader-spinner';
 
-const Spinner = () => {
+const Spinner = ({color}) => {
   return (
     <Watch
   height="80"
   width="80"
   radius="48"
-  color="#4fa94d"
+  color={color && color || '#73fc03'}
   ariaLabel="watch-loading"
   wrapperStyle={{}}
   wrapperClassName=""
