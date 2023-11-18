e70bce42e9b3d98ab4cd525834103f792fe7f869
Mohammed Suhail PM <mohammedpm@meta.com>
2023-11-18 22:47:23 +0530

    OTP on register as admin done

--- app/admin/register/page.tsx
+++ app/admin/register/page.tsx
@@ -4,7 +4,7 @@ import {initiateRegister} from '@actions'
 
 const RegisterAsAdmin = () => {
     const [formData, setFomData] = useState({
-        userName: '',
+        email: '',
         password: '',
         confirmPassword: '',
     })
@@ -30,16 +30,16 @@ console.log(formData)
         <div className='bg-blue-300/50 h-screen pt-4'>
             <h1 className='text-center font-bold uppercasef'>Register as admin</h1>
             <form action="" className='flex flex-col items-center mt-4 font-sans' onSubmit={handleRegister}>
-                <label className='font-bold' htmlFor="username">Username</label>
-                <input type="text" name='userName' value={formData.userName} placeholder='Recommended to use mobile number' className='w-[300px] pl-4 pr-4 p-1 border border-green-500 rounded-sm' required
+                <label className='font-bold' htmlFor="email">Email</label>
+                <input type="text" name='email' value={formData.email} placeholder='Recommended to use mobile number' className='w-[300px] pl-4 pr-4 p-1 border border-green-500 rounded-sm' required
                     onChange={handleChange} />
                 <label className='font-bold' htmlFor="password">Password</label>
                 <input type="password" name='password' value={formData.password} placeholder='Enter your password' className='w-[300px] pl-4 pr-4 p-1 border border-green-500 rounded-sm' required  onChange={handleChange}/>
                 <label className='font-bold' htmlFor="password">Confirm Password</label>
                 <input type="password" name='confirmPassword' value={formData.confirmPassword} placeholder='Re-enter your password' className='w-[300px] pl-4 pr-4 p-1 border border-green-500 rounded-sm' required  onChange={handleChange} />
 
-                <button disabled={!((formData.userName) && (formData.password) && (formData.password === formData.confirmPassword))} type='submit' className='border border-none bg-green-600 text-yellow-100 m-4 pl-4 pr-4 p-2 rounded-md hover:bg-green-500 hover:border-white'>Register</button>
-            </form> 
+                <button disabled={!((formData.email) && (formData.password) && (formData.password === formData.confirmPassword))} type='submit' className='border border-none bg-green-600 text-yellow-100 m-4 pl-4 pr-4 p-2 rounded-md hover:bg-green-500 hover:border-white'>Register</button>
+            </form>
         </div>
     )
 }
--- app/api/admin/register/route.js
+++ app/api/admin/register/route.js
@@ -9,33 +9,37 @@ const durationInMinutes = 10;
 export const POST = async (req) => {
   try {
     await connectToDB();
-    const body = await req.json();
-    const { userName, password } = body;
-    console.log(userName, password);
+    const formData = await req.json();
+    const { email, password } = formData;
+    console.log(email, password); 
     console.log(" this is from api route");
     // return if the user aleady exists
-    const alreadyRegistered = await Admin.findOne({ email: userName });
+    const alreadyRegistered = await Admin.findOne({ email });
     if (alreadyRegistered) {
       return new Response(
         JSON.stringify({ message: "Admin is already registered" }),
         { status: 400 }
       );
     }
-    const newOTPGenerated = await generateOTP();
+    const newOTPGenerated = generateOTP();
     console.log(newOTPGenerated, ' is correct OTP')
-    
+
     const hashedOTP = await bcrypt.hash(newOTPGenerated.toString(), 10);
     console.log(hashedOTP, " is hashedOTP");
+    const newOTPObject = ({
+      email,
+      otp: newOTPGenerated,
+    })
     const newOTPDoc = new OTP({
-      email: userName,
+      email,
       otp: hashedOTP,
       createdAt: Date.now(),
       expiresAt: Date.now() + durationInMinutes * 60 * 1000,
     });
     await newOTPDoc.save();
-    await sendEmail({...newOTPDoc, otp: newOTPGenerated});
+    await sendEmail({email,otp: newOTPGenerated});
     return new Response(
-      JSON.stringify({ message: `OTP sent over ${userName}` }),
+      JSON.stringify({ message: `OTP sent over ${email}` }),
       { status: 200 }
     );
   } catch (error) {
