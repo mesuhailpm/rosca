8e02ab18b43fcb6c26ecadb9aa00be338eb34cb0
Mohammed Suhail PM <mohammedpm@meta.com>
2023-11-19 00:00:32 +0530

    on the way for verify OTP ;implement a page to enter OTP

--- app/admin/register/page.tsx
+++ app/admin/register/page.tsx
@@ -1,6 +1,8 @@
 'use client'
 import React, { ChangeEvent, FormEvent, useState } from 'react'
 import {initiateRegister} from '@actions'
+import Confirmation from '@components/Confirmation'
+import Spinner from '@components/Spinner'
 
 const RegisterAsAdmin = () => {
     const [formData, setFomData] = useState({
@@ -8,27 +10,44 @@ const RegisterAsAdmin = () => {
         password: '',
         confirmPassword: '',
     })
+    const [loading, setLoading] = useState(false)
+    const [showConfirmation,setShowConfirmation] = useState(false)
+    const [confirmationMessage, setConfirmationMessage] = useState ({message:'', success: false})
     const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
         setFomData((prevState) => {
             return { ...prevState, [event.target.name]: event.target.value }
 
         })
     }
-    const handleRegister = (event:FormEvent<HTMLFormElement>) => {
+    const handleRegister =  async(event:FormEvent<HTMLFormElement>) => {
         event.preventDefault()
         try {
             //...handle register
-            initiateRegister(formData)
+            setLoading(true)
+            const {message} = await initiateRegister(formData)
+            console.log(message)
+            setConfirmationMessage({message, success: true})
+            setLoading(false)
+            setShowConfirmation(true)
+            const storedUser :{} | null = localStorage.getItem('userObject')
+            localStorage.setItem('userObject', JSON.stringify({
+                ...storedUser,
+                pendingAdmin:formData.email,
+            }))
+            setTimeout(()=>{
+                pathname
+
+            },1000)
+
         } catch (error) {
             console.error(error)
         }
 
     }
-console.log(formData)
 
     return (
         <div className='bg-blue-300/50 h-screen pt-4'>
-            <h1 className='text-center font-bold uppercasef'>Register as admin</h1>
+            <h1 className='text-center font-bold uppercase'>Register as admin</h1>
             <form action="" className='flex flex-col items-center mt-4 font-sans' onSubmit={handleRegister}>
                 <label className='font-bold' htmlFor="email">Email</label>
                 <input type="text" name='email' value={formData.email} placeholder='Recommended to use mobile number' className='w-[300px] pl-4 pr-4 p-1 border border-green-500 rounded-sm' required
@@ -40,6 +59,13 @@ console.log(formData)
 
                 <button disabled={!((formData.email) && (formData.password) && (formData.password === formData.confirmPassword))} type='submit' className='border border-none bg-green-600 text-yellow-100 m-4 pl-4 pr-4 p-2 rounded-md hover:bg-green-500 hover:border-white'>Register</button>
             </form>
+            {loading && (
+                <div className='fixed top-0 right-0 flex flex-col gap-4 bg-gray-200/50 items-center w-screen h-screen justify-center items-center'> <Spinner color='#000000'/><h1 className='text-black font-bold'>Validating the OTP...</h1></div>
+            )}
+            {showConfirmation && (
+                                <div className='fixed top-0 right-0 flex flex-col gap-4 bg-gray-200/75 items-center w-screen h-screen justify-center items-center'> <Confirmation confirmationMessage={confirmationMessage}/><h1 className='text-black font-bold'></h1></div>
+
+            )}
         </div>
     )
 }
