63844b58ee22c85d08778eb62e040fe738ebe077
Mohammed Suhail PM <mohammedpm@meta.com>
2023-12-19 05:41:30 +0530

    .

--- components/DeleteModal.tsx
+++ components/DeleteModal.tsx
@@ -2,18 +2,19 @@
 import { useStore } from '@src/store'
 import { State } from '@types'
 import React from 'react'
+import handleSubmit from '@hooks/useSubmitForm'
 
 
 
 const DeleteModal = () => {
-  const  {toggleShowDeleteModal, idTodelete, showDeleteModal, handleSubmit} = useStore() as State
+  const  {toggleShowDeleteModal, idTodelete, showDeleteModal} = useStore() as State
   if(showDeleteModal)
   return (
     <div className='w-screen h-screen fixed top-0 left-0 border border-black justify-center flex flex-col gap-1 p-4 rounded-md items-center'>
       <div className='bg-orange-400 flex  flex-col  gap-2 rounded-2xl justify-center items-center p-4'>
 
-        <h1 className=''>Are you sure delete the member</h1>
-        <button className='bg-red-800 text-white w-3/5 rounded-lg' onClick={()=>handleSubmit(null,'remove',idTodelete)}>Yes</button>
+        <h1 className=''>Are you sure delete the member?</h1>
+        <button className='bg-red-800 text-white w-3/5 rounded-lg' onClick={()=>handleSubmit()}>Yes</button>
         <button className='bg-blue-800 text-white w-3/5 rounded-lg' onClick={toggleShowDeleteModal}>No</button>
       </div>
 
