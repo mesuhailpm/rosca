aec2a9f17c46af9245b301e462d468afffe10a6a
Mohammed Suhail PM <mohammedpm@meta.com>
2023-12-26 03:21:25 +0530

    OTPInput as reusable, js --> ts,

--- app/admin/verify/page.tsx
+++ app/admin/verify/page.tsx
@@ -4,11 +4,12 @@ import OTPInput from "react-otp-input";
 import { verifyOTP, createAdmin } from "@actions";
 import Confirmation from "@components/Confirmation";
 import { useStore } from "@src/store";
+import OTPInputField from "@components/OTPInputField";
 const numberOfInputs = 6;
 
 const Verify = () => {
+  const [otp, setOtp] = useState<string>("");
   const { runConfirmation } = useStore()
-  const [otp, setOtp] = useState<number>(0);
 
   const [storedUserObject, setStoredUserObject] = useState({
     pendingAdmin: "unauthorized",
@@ -17,7 +18,7 @@ const Verify = () => {
 
   const [initialLoading, setInitialLoading] = useState(true);
   const handleChange = (otp: string) => {
-    setOtp(Number(otp));
+    setOtp(otp);
   };
 
 
@@ -46,16 +47,14 @@ const Verify = () => {
         setTimeout(async () => {
           //create a new admin with password
           const data = await createAdmin({ email: pendingAdmin });
-          if (data.success) {
+          
             console.log(data);
             runConfirmation({ message: data.message, success: true }, 1000)
             setTimeout(() => {
               location.href = "/admin/login";
             }, 1000);
-          } else {
-            console.log();
-            runConfirmation({ message: data.message, success: false });
-          }
+           
+            console.log();          
         }, 2000);
       }else{
         console.log("otp validation failed");
@@ -107,34 +106,14 @@ const Verify = () => {
 
   return (
     <div className="flex flex-col items-center bg-gray-200 p-2">
-      <h1 className="text-xl font-bold">Enter the OTP</h1>
-      <form
-        className="p-4 m-2 flex flex-col items-center"
-        onSubmit={handleSubmit}
-      >
-        <label htmlFor="otp">Enter OTP</label>
-        <OTPInput
-          value={otp.toString()}
-          onChange={handleChange}
-          numInputs={numberOfInputs}
-          renderInput={(props) => (
-            <input {...props} className="m-2" inputMode="numeric" />
-          )}
-          inputStyle={{
-            width: "20px"
-          }}
-          shouldAutoFocus={true}
-        />
-        <button
-          className="bg-blue-800 text-fuchsia-100 p-2 m-2 hover:bg-blue-700  rounded-md"
-          type="submit"
-        >
-          Submit
-        </button>
-      </form>
-      
-          <Confirmation />
-
+      <OTPInputField 
+      email={storedUserObject.pendingAdmin}
+      expiresIn={10}
+      handleChange={handleChange}
+      handleSubmit={handleSubmit}
+      numberOfInputs={6}
+      otp={otp}
+      />
     </div>
   );
 };
