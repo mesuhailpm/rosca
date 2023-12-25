b65ad134efdf4e90aed491fe422e0b37d6158a5f
Mohammed Suhail PM <mohammedpm@meta.com>
2023-12-26 03:32:30 +0530

    auth js --> ts

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 const secret = "secret";
-export const generateToken = async (userName) => {
+export const generateToken = async (userName: string) => {
   try {
     const token = await jwt.sign({userName,test: true}, secret,{expiresIn: '1hr'});//expiresIn can't be set when payload is simple like string
     // console.log(token,' is token')
