f0788ab813b55c2f15811c324ecb0e2cdc8ddfe6
Mohammed Suhail PM <mohammedpm@meta.com>
2023-12-26 03:31:00 +0530

    added optional caption in loader

--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -10,21 +10,24 @@ type SpinnerProps = {
 }
 
 const Spinner = ({ color }: SpinnerProps) => {
-  const { responseLoading } = useStore() as State
+  const { responseLoading, loadingCaption } = useStore() as State
 
   if (responseLoading) {
     return (
-      <Watch
-        height="80"
-        width="80"
-        radius="48"
-        color={color && color || '#73fc03'}
-        ariaLabel="watch-loading"
-        wrapperStyle={{}} //Styles to be applied to the wrapper. It should be a valid CSS object and can be used for custom styling. It will override the default style.
-        //wrapperClassName={undefined} // ClassName to be applied to the wrapper. It can be used for custom styling and will override the default style. This prop is expected as documentation but not working. Created a GH issue.https://github.com/mhnpd/react-loader-spinner/issues/164
-        visible={true}
-        wrapperClass='fixed w-screen h-screen border border-black loading top-0 right-0 flex justify-center items-center'
-      />
+      <div className='fixed flex flex-col items-center justify-center gap-2 bg-slate-100/40 left-0 top-0 w-screen h-screen'>
+        <Watch
+          height="80"
+          width="80"
+          radius="48"
+          color={color && color || '#73fc03'}
+          ariaLabel="watch-loading"
+          wrapperStyle={{}} //Styles to be applied to the wrapper. It should be a valid CSS object and can be used for custom styling. It will override the default style.
+          //wrapperClassName={undefined} // ClassName to be applied to the wrapper. It can be used for custom styling and will override the default style. This prop is expected as documentation but not working. Created a GH issue. https://github.com/mhnpd/react-loader-spinner/issues/164
+          visible={true}
+          wrapperClass='loading top-1/2 left-1/2'
+        />
+        <p className='text-2xl font-bold italic'>{loadingCaption}</p>
+      </div>
     )
   } else {
     return null
