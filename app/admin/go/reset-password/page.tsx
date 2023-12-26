7e34c39a888de69e085f8c6cb2dfd8f689dfd7d5
suhailpm <tosuhailpm@gmail.com>
2023-12-26 17:57:26 +0530

    admin/reset-password to admin/go/rest-password

--- app/admin/go/reset-password/page.tsx
+++ app/admin/go/reset-password/page.tsx
@@ -47,7 +47,7 @@ const ResetAdminPassword = () => {
             setShowConfirmation(true)
 
             setTimeout(() => {
-                location.href = 'login'
+                location.href = 'admin/go/login'
 
             }, 1000)
 
