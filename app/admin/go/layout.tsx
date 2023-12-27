85cd31cb6f28c3292da95c5eea24392d5855ecf9
Mohammed Suhail PM <mohammedpm@meta.com>
2023-12-28 04:58:17 +0530

    workaround for error React component names must start with an uppercase letter.

--- app/admin/go/layout.tsx
+++ app/admin/go/layout.tsx
@@ -6,6 +6,7 @@ import LoaderSpinner from '@components/Spinner'
 import checkLoggedIn from '@utils/checkLoggedIn'
 import { useStore } from '@src/store'
 import { State } from '@types'
+import Layout from '@components/Layout'
 
 
 const layout = ({ children }: { children: React.ReactNode }) => {
@@ -36,6 +37,12 @@ const layout = ({ children }: { children: React.ReactNode }) => {
 
     return (
         <div className='p-2 '>
+            {/* Adding a Layout as component as building fails on Vercel as useStore can be used only on custom hooks and
+            React Hook "useStore" is called in function "layout" that is neither a React function component nor a custom React Hook function.
+            React component names must start with an uppercase letter. React Hook names must start with the word "use".  react-hooks/rules-of-hooks
+            */}
+            <Layout />
+
             {children}
 
             <Confirmation />
