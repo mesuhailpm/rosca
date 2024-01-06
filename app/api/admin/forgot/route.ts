fdefd7413f32593a1997477e719299c638961362
suhailpm <tosuhailpm@gmail.com>
2024-01-06 21:45:47 +0530

    component wise loading removed, layout changes, added success property to backend,

--- app/api/admin/forgot/route.ts
+++ app/api/admin/forgot/route.ts
@@ -6,23 +6,24 @@ import Admin from "@models/Admin";
 import bcrypt from "bcrypt";
 import { NextRequest } from "next/server";
 const durationInMinutes = 10;
-const {NEXT_PUBLIC_REGISTRATION_ALLOWED } = process.env;
+const { NEXT_PUBLIC_REGISTRATION_ALLOWED } = process.env;
 
 export const POST = async (req: NextRequest) => {
   if (NEXT_PUBLIC_REGISTRATION_ALLOWED === 'false') {
     console.log('NEXT_PUBLIC_REGISTRATION_ALLOWED is \'false\' please update it.')
     return new Response(
-    JSON.stringify({ message: "Register is diabled. Please contact the owner" }),
-    { status: 501 }
-  );}
+      JSON.stringify({ message: "Register is diabled. Please contact the owner" }),
+      { status: 501 }
+    );
+  }
   try {
     await connectToDB();
     const formData = await req.json();
-    const { email} = formData;
-    console.log(email); 
+    const { email } = formData;
+    console.log(email);
     console.log(" this is from api route inside forgot");
     // return if the user aleady exists
-    const alreadyRegistered = await Admin.findOne({ userName:email });
+    const alreadyRegistered = await Admin.findOne({ userName: email });
     if (!alreadyRegistered) {
       console.log('no admin to verify');
       return new Response(
@@ -30,9 +31,9 @@ export const POST = async (req: NextRequest) => {
         { status: 400 }
       );
     }
-    const existingOTP = await OTP.find({email})
+    const existingOTP = await OTP.find({ email })
     console.log(existingOTP);
-    if(existingOTP) await OTP.findOneAndRemove({email})
+    if (existingOTP) await OTP.findOneAndRemove({ email })
     const newOTPGenerated = generateOTP();
     console.log(newOTPGenerated, ' is correct OTP')
     const hashedOTP = await bcrypt.hash(newOTPGenerated.toString(), 10);
@@ -46,18 +47,18 @@ export const POST = async (req: NextRequest) => {
       expiresAt: Date.now() + durationInMinutes * 60 * 1000,
     });
     await newOTPDoc.save();
-    await sendEmail({email,otp: newOTPGenerated});
+    await sendEmail({ email, otp: newOTPGenerated });
     console.log('sending fincal response');
     return new Response(
-      JSON.stringify({ message: `OTP sent over ${email}` }),
+      JSON.stringify({ message: `OTP sent over ${email}`, success: true }),
       //sending the email in coookie
-      { status: 200,headers:{"Set-Cookie":`pendingEmail=${email}; Http-Only; Max-Age:3600` }}
+      { status: 200, headers: { "Set-Cookie": `pendingEmail=${email}; Http-Only; Max-Age:3600` } }
     );
   } catch (error) {
     console.log(error, " is the error in register api route");
     return new Response(
-      JSON.stringify({ message: `something went wrong`}),
-      { status: 403}
+      JSON.stringify({ message: `something went wrong`, }),
+      { status: 403 }
     )
   }
 };
