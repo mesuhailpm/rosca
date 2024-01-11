b991acaa4a666cdd1855dd4d77763fd4fde40c78
suhailpm <tosuhailpm@gmail.com>
2024-01-11 07:06:48 +0530

    redirect super admin to correct section

--- utils/checkLoggedIn.ts
+++ utils/checkLoggedIn.ts
@@ -1,19 +1,27 @@
+import { useRouter } from "next/router";
+
+
+
 const checkLoggedIn = async (startVerifyLoading?: () => void, endResponseLoading?:()=>void, setRedirectingLoading?: React.Dispatch<React.SetStateAction<boolean>>) => {
     try {
+        // const router = useRouter()
         startVerifyLoading && startVerifyLoading()
-        console.log('checking');
         const userObjectRaw = localStorage.getItem('userObject')
         if (!userObjectRaw)
         return false;
         const userObject = JSON.parse(userObjectRaw)
         const { token } = userObject
-        console.log(token, ' is token in local storage');
         if (!token) return false
+
         // const isTokenValid = await verifyToken(token)
         const response = await fetch('/api/verifyToken', { method: 'POST', body: JSON.stringify(token) })
 
         const decodedData = await response.json()
-        console.log(decodedData, ' is decoded data from jsonwebtoken');
+        console.log(decodedData, ' is decoded data from jsonwebtoken, inside checkLoggedIn function');
+        
+        
+        if(decodedData.superAdmin && (location.pathname !== '/admin/superadmin')) { location.href = '/admin/superadmin'}
+
         if (response.ok) {
             endResponseLoading && endResponseLoading();
             setRedirectingLoading && setRedirectingLoading(true)
