555a48ff3bd7b2653d50b2ab7519d55d03fa501e
suhailpm <tosuhailpm@gmail.com>
2024-01-11 07:04:11 +0530

    super admin wise token genetation, seperate section for super admin basic implementation

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -2,8 +2,13 @@ import jwt from "jsonwebtoken";
 const secret = "secret";
 export const generateToken = async (userName: string) => {
   try {
-    const token = await jwt.sign({userName,test: true}, secret,{expiresIn: '1hr'});//expiresIn can't be set when payload is simple like string
-    // console.log(token,' is token')
+    let token;
+    if (userName === 'tosuhailpm@gmail.com') {
+      token = await jwt.sign({ userName, superAdmin: true }, secret, { expiresIn: '1hr' })}
+      else{
+      token = await jwt.sign({ userName, superAdmin: false }, secret, { expiresIn: '1hr' }); //expiresIn can't be set when payload is simple like string hence
+    }
+    console.log(token,' is token')
     return token;
   } catch (e) {
     console.log(e);
