a47b5fe71ddb70624d99e05ba8ba435114c7ccd0
suhailpm <tosuhailpm@gmail.com>
2024-09-22 02:13:16 +0530

    fix: if there no token provided the app fails on production

--- app/api/verifyToken/route.ts
+++ app/api/verifyToken/route.ts
@@ -1,21 +1,24 @@
 import jwt from 'jsonwebtoken'
 import { NextRequest } from 'next/server';
-const {TOKEN_SECRET} = process.env
-export const POST = async(req: NextRequest) => {
-    const token = await req.json()
-    try {
+const { TOKEN_SECRET } = process.env
+export const POST = async (req: NextRequest) => {
+  const token = await req.json()
+  console.log(TOKEN_SECRET, 'is token')
+  console.log(token, 'is token')
+  try {
 
-      if(!TOKEN_SECRET){throw Error('No Secret Provided For Token!')}
-
-        // console.log(body, ' is jsonwebtoken body')
-      // console.log(token, 'is token to verify');
-      const decoded = await jwt.verify(token, TOKEN_SECRET)
-      // console.log(decoded, ' i sdecoded');
-      if (decoded) return new Response(JSON.stringify(decoded),{status: 200})
-    } catch (e) {
-      console.log(e);
-      return new Response(JSON.stringify(e),{status:401})
-
+    if (!TOKEN_SECRET) {
+      return new Response(JSON.stringify({ decoded: false, message: 'No Secret Provided For Token!' }), { status: 200 })
     }
+    // console.log(body, ' is jsonwebtoken body')
+    // console.log(token, 'is token to verify');
+    const decoded = await jwt.verify(token, TOKEN_SECRET)
+    console.log(decoded, ' i sdecoded');
+    if (decoded) { return new Response(JSON.stringify(decoded), { status: 200 }) }
+
+  } catch (e) {
+    console.log(e);
+    return new Response(JSON.stringify(e))
+
   }
-  
+}
