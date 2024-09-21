0b81811097bcc9521a4500ec366dba4719e03fb0
suhailpm <tosuhailpm@gmail.com>
2024-09-22 01:06:52 +0530

    removed confidential values from git history and recreated files and moved into .env

--- app/api/verifyToken/route.ts
+++ app/api/verifyToken/route.ts
@@ -1,11 +1,15 @@
 import jwt from 'jsonwebtoken'
 import { NextRequest } from 'next/server';
+const {TOKEN_SECRET} = process.env
 export const POST = async(req: NextRequest) => {
     const token = await req.json()
     try {
+
+      if(!TOKEN_SECRET){throw Error('No Secret Provided For Token!')}
+
         // console.log(body, ' is jsonwebtoken body')
       // console.log(token, 'is token to verify');
-      const decoded = await jwt.verify(token, 'secret')
+      const decoded = await jwt.verify(token, TOKEN_SECRET)
       // console.log(decoded, ' i sdecoded');
       if (decoded) return new Response(JSON.stringify(decoded),{status: 200})
     } catch (e) {
